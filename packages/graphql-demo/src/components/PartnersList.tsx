9f054d1b4afd4190d173d1c5552111f476827d29
Martin Petit <martin@igloo.be>
2019-05-20 12:36:55 +0200

    graphql-demo: introduce pagination for partners list

--- packages/graphql-demo/src/components/PartnersList.tsx
+++ packages/graphql-demo/src/components/PartnersList.tsx
@@ -1,26 +1,45 @@
 import gql from "graphql-tag";
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-apollo-hooks";
 import { Loading } from "../layout/Loading";
-import { GetPartners } from "./__generated__/GetPartners";
+import { GetPartners, GetPartnersVariables } from "./__generated__/GetPartners";
+import { PartnersListForm } from "./PartnersListForm";
 
 const GetPartnersQuery = gql`
-  query GetPartners {
-    partnerList {
-      extId
-      pubName
-      street1
-      streetnum
-      postalCode
-      city
-      country
+  query GetPartners($page: Float, $limit: Float) {
+    partnerList(page: $page, limit: $limit) {
+      pageInfo {
+        hasNextPage
+        count
+        page
+        limit
+      }
+      result {
+        extId
+        pubName
+        street1
+        streetnum
+        postalCode
+        city
+        country
+      }
     }
   }
 `;
 
 export const PartnersList = () => {
-  const { data, loading } = useQuery<GetPartners>(GetPartnersQuery, {
+  const [connectionArgs, setConnectionArgs] = useState({
+    page: 1,
+    limit: 10
+  })
+  const { data, loading } = useQuery<
+    GetPartners,
+    GetPartnersVariables
+  >(GetPartnersQuery, {
     fetchPolicy: "cache-and-network",
+    variables: {
+      ...connectionArgs,
+    },
   });
 
   const isLoading = !data && loading;
@@ -28,33 +47,59 @@ export const PartnersList = () => {
   return (
     <>
       <h2>Partner list</h2>
+      <PartnersListForm setConnectionArgs={setConnectionArgs} />
       {data && data.partnerList && !isLoading ? (
-        <table>
-          <thead>
-            <tr>
-              <th>extId</th>
-              <th>pubName</th>
-              <th>street1</th>
-              <th>streetnum</th>
-              <th>postalCode</th>
-              <th>city</th>
-              <th>country</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.partnerList.map(partner => (
-              <tr key={partner.extId}>
-                <td>{partner.extId}</td>
-                <td>{partner.pubName}</td>
-                <td>{partner.street1}</td>
-                <td>{partner.streetnum}</td>
-                <td>{partner.postalCode}</td>
-                <td>{partner.city}</td>
-                <td>{partner.country}</td>
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>page</th>
+                <th>total pages</th>
+                <th>limit</th>
+                <th>total item</th>
+                <th>has next page</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{data.partnerList.pageInfo.page}</td>
+                <td>
+                  {Math.ceil(
+                    data.partnerList.pageInfo.count /
+                      data.partnerList.pageInfo.limit,
+                  )}
+                </td>
+                <td>{data.partnerList.pageInfo.limit}</td>
+                <td>{data.partnerList.pageInfo.count}</td>
+                <td>{data.partnerList.pageInfo.hasNextPage.toString()}</td>
+              </tr>
+            </tbody>
+            <thead>
+              <tr>
+                <th>extId</th>
+                <th>pubName</th>
+                <th>street1</th>
+                <th>streetnum</th>
+                <th>postalCode</th>
+                <th>city</th>
+                <th>country</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.partnerList.result.map(partner => (
+                <tr key={partner.extId}>
+                  <td>{partner.extId}</td>
+                  <td>{partner.pubName}</td>
+                  <td>{partner.street1}</td>
+                  <td>{partner.streetnum}</td>
+                  <td>{partner.postalCode}</td>
+                  <td>{partner.city}</td>
+                  <td>{partner.country}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       ) : (
         <Loading />
       )}
---
+++ packages/graphql-demo/src/components/PartnersListForm.tsx
@@ -0,0 +1,32 @@
+import { Field, Form, Formik, FormikValues } from "formik";
+import React from "react";
+import { SimpleInput } from "../layout/SimpleInput";
+
+export const PartnersListForm = ({
+  setConnectionArgs,
+}: {
+  setConnectionArgs: React.Dispatch<React.SetStateAction<{
+    page: number;
+    limit: number;
+}>>
+}) => (
+  <Formik
+    onSubmit={(values, { setSubmitting }) => {
+      setConnectionArgs({
+          ...values,
+        });
+      setSubmitting(false);
+    }}
+    initialValues={{
+      page: 1,
+      limit: 10,
+    }}
+    render={() => (
+      <Form>
+        <Field name="page" type="number" component={SimpleInput} />
+        <Field name="limit" type="number" component={SimpleInput} />
+        <button type="submit">Submit</button>
+      </Form>
+    )}
+  />
+);
