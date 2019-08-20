f00e96642db5a43cb81d7e68ddfedd71454f3b9b
Martin Petit <martin@igloo.be>
2019-08-20 18:12:48 +0200

    graphql-schema: direct cashbard: move `listEligibleDirectCashbackDeals` resolver

--- packages/graphql-schema/src/directCashback/DirectCashbackResolver.ts
+++ packages/graphql-schema/src/directCashback/DirectCashbackResolver.ts
@@ -154,6 +154,27 @@ export class DirectCashbackResolver {
     return new DirectCashbackDealDetail(res);
   }
 
+  @Query(() => [DirectCashbackDealDetail], { nullable: false })
+  public async listEligibleDirectCashbackDeals(
+    @Ctx() ctx: Context,
+    @Arg("token") token: string,
+  ): Promise<DirectCashbackDealDetail[]> {
+    const { user_guid } = ctx.userTokenHelper.parse(token);
+    const res = await ctx.omnipartners.deals.listEligibleDirectCashbackDeals({
+      user_guid,
+    });
+    return Promise.all(
+      res.data.map(
+        async ({ ref }) =>
+          new DirectCashbackDealDetail(
+            (await ctx.omnipartners.deals.getDirectCashbackDealDetail({
+              ref,
+            })).data,
+          ),
+      ),
+    );
+  }
+
   @Mutation(() => GenericValidationError, { nullable: true })
   public async directCashbackDealSubscribe(
     @Ctx() ctx: Context,
