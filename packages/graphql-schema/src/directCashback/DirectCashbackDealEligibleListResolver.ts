4ef3464803a68cfe10bd95e427b3c414145cb862
Martin Petit <martin@igloo.be>
2019-08-20 17:52:42 +0200

    graphql-schema: remove `UserEligibleDirectCashbackDeal` for `DirectCashbackDealDetail`

--- packages/graphql-schema/src/directCashback/DirectCashbackDealEligibleListResolver.ts
+++ packages/graphql-schema/src/directCashback/DirectCashbackDealEligibleListResolver.ts
@@ -1,29 +1,26 @@
 import { Arg, Ctx, Query } from "type-graphql";
 import { Context } from "../types/Context";
-import { UserEligibleDirectCashbackDeal } from "../user/UserEligibleDirectCashbackDeal";
+import { DirectCashbackDealDetail } from "./DirectCashbackResolver";
 
 export class DirectCashbackDealEligibleListResolver {
-  @Query(() => [UserEligibleDirectCashbackDeal], { nullable: false })
+  @Query(() => [DirectCashbackDealDetail], { nullable: false })
   public async listEligibleDirectCashbackDeals(
     @Ctx() ctx: Context,
     @Arg("token") token: string,
-  ): Promise<UserEligibleDirectCashbackDeal[]> {
+  ): Promise<DirectCashbackDealDetail[]> {
     const { user_guid } = ctx.userTokenHelper.parse(token);
     const res = await ctx.omnipartners.deals.listEligibleDirectCashbackDeals({
       user_guid,
     });
     return Promise.all(
-      res.data.map(async d => {
-        const r = await ctx.omnipartners.deals.getDirectCashbackDealDetail({
-          ref: d.ref,
-        });
-
-        return new UserEligibleDirectCashbackDeal({
-          slogan: r.data.slogan,
-          publicName: r.data.public_name,
-          ...d,
-        });
-      }),
+      res.data.map(
+        async ({ ref }) =>
+          new DirectCashbackDealDetail(
+            (await ctx.omnipartners.deals.getDirectCashbackDealDetail({
+              ref,
+            })).data,
+          ),
+      ),
     );
   }
 }
