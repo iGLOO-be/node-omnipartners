68af85876ba05e702ecc2abf458ce139d7d3dc26
Andy Vanbutsele <andy.vanbutsele@gmail.com>
2019-10-16 15:57:56 +0200

    @graphql-schema: allow authenticateError custom message

--- packages/graphql-schema/src/lib/AuthenticationError.ts
+++ packages/graphql-schema/src/lib/AuthenticationError.ts
@@ -1,4 +1,9 @@
 export class AuthenticationError extends Error {
   public message = "Not logged!";
   public code = "E_NOT_LOGGED";
+
+  constructor(message = "Not logged!") {
+    super()
+    this.message = message
+  }
 }
