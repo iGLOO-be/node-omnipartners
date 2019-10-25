aca8eb4b5373d2575a30feae16c1b8a2617e53de
Loïc Mahieu <mahieuloic@gmail.com>
2019-10-25 14:36:40 +0200

    hooks: user: pets: always return an array

--- packages/hooks/src/user/pets.ts
+++ packages/hooks/src/user/pets.ts
@@ -71,7 +71,7 @@ export const useUserPets = () => {
       res.data &&
       res.data.user &&
       res.data.user.result &&
-      res.data.user.result.pets,
+      res.data.user.result.pets || [],
   };
 };
 
