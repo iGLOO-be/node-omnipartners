89585eefabdb57457fe217fb2adf5e78bbd2ab4e
Loïc Mahieu <mahieuloic@gmail.com>
2019-10-25 15:59:42 +0200

    hooks: unify children

---
+++ packages/hooks/src/user/__generated__/UserChildrenCreate.ts
@@ -0,0 +1,67 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { UserChildCreateInput } from "./../../../__generated__/globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: UserChildrenCreate
+// ====================================================
+
+export interface UserChildrenCreate_userChildCreate_result_user_owner {
+  __typename: "UserOwner";
+  guid: string;
+}
+
+export interface UserChildrenCreate_userChildCreate_result_user_children {
+  __typename: "UserChild";
+  birthday: any | null;
+  gender: string;
+  guid: string;
+  firstName: string;
+}
+
+export interface UserChildrenCreate_userChildCreate_result_user {
+  __typename: "User";
+  owner: UserChildrenCreate_userChildCreate_result_user_owner;
+  children: UserChildrenCreate_userChildCreate_result_user_children[];
+}
+
+export interface UserChildrenCreate_userChildCreate_result {
+  __typename: "UserAndChild";
+  user: UserChildrenCreate_userChildCreate_result_user;
+}
+
+export interface UserChildrenCreate_userChildCreate_error_validationErrors_errors {
+  __typename: "ValidationError";
+  validator: string;
+  message: string;
+}
+
+export interface UserChildrenCreate_userChildCreate_error_validationErrors {
+  __typename: "FieldValidationError";
+  field: string;
+  errors: UserChildrenCreate_userChildCreate_error_validationErrors_errors[];
+}
+
+export interface UserChildrenCreate_userChildCreate_error {
+  __typename: "GenericValidationError";
+  message: string;
+  code: string;
+  validationErrors: UserChildrenCreate_userChildCreate_error_validationErrors[] | null;
+}
+
+export interface UserChildrenCreate_userChildCreate {
+  __typename: "UserChildResult";
+  result: UserChildrenCreate_userChildCreate_result | null;
+  error: UserChildrenCreate_userChildCreate_error | null;
+}
+
+export interface UserChildrenCreate {
+  userChildCreate: UserChildrenCreate_userChildCreate;
+}
+
+export interface UserChildrenCreateVariables {
+  userChildCreateInput: UserChildCreateInput;
+  token: string;
+}
