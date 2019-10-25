e1a9377a504e56bb8821de8022181a0874a6ed5f
Loïc Mahieu <mahieuloic@gmail.com>
2019-10-25 13:04:48 +0200

    hooks: add metadata animal breeds

---
+++ packages/hooks/src/metadata/__generated__/MetadataAnimalBreeds.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MetadataAnimalBreeds
+// ====================================================
+
+export interface MetadataAnimalBreeds_metadataAnimalBreeds {
+  __typename: "AnimalBreed";
+  id: string;
+  name: string;
+}
+
+export interface MetadataAnimalBreeds {
+  metadataAnimalBreeds: MetadataAnimalBreeds_metadataAnimalBreeds[];
+}
+
+export interface MetadataAnimalBreedsVariables {
+  lang: string;
+}
--- packages/hooks/src/metadata/index.ts
+++ packages/hooks/src/metadata/index.ts
@@ -2,3 +2,4 @@ export * from "./useMetadataUserTitles";
 export * from "./useMetadataLegalForms";
 export * from "./useMetadataCountries";
 export * from "./useMetadataSubscriptions";
+export * from "./useMetadataAnimalBreeds";
