c416c0bf6f093e25ecaf791ed2e76057063e3f12
Loïc Mahieu <mahieuloic@gmail.com>
2019-10-25 12:13:54 +0200

    hooks: remove `useAsyncStorage`

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -4,5 +4,4 @@ export * from "./partners/useFetchPartnerLocator";
 export * from "./partners/usePartnerDetails";
 export * from "./deals/useDealCheckSecureCode";
 export * from "./deals/useDealSubscribe";
-export * from "./lib/useAsyncStorage";
 export * from "./lib/dataIdFromObject";
