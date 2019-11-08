0c2bfdb3170db96022abae4285b969024d6f9bd7
Loïc Mahieu <mahieuloic@gmail.com>
2019-11-08 11:49:24 +0100

    omnipartners: loyalty: move specific error map `1044` to default

--- packages/omnipartners/src/api/loyalty/index.ts
+++ packages/omnipartners/src/api/loyalty/index.ts
@@ -94,6 +94,7 @@ export default class Loyalty extends Api {
     1027: { message: "Invalid Secret Key" },
     1042: { message: "Resolve the Card Number - Invalid Request" },
     1043: { message: "Resolve the Card Number / Mobile Number - User not found" },
+    1044: { message: "Associated user account is not active" },
     1054: { message: "Authentication key and action not mapped" },
     1055: {
       message: "ptn_ext_customer_id ( partner_ext_id ) record not found",
@@ -122,7 +123,6 @@ export default class Loyalty extends Api {
   ): Promise<ILoyaltyBalance> {
     return this._call("getpointbalance", data, {
       errorMap: {
-        1044: { message: "Associated user account is not active" },
         1045: { message: "Resolve the Card Number - Card not found" },
         1046: { message: "Resolve the Card Number - Card expired" },
         1091: { message: "Default program is not set in control center." },
@@ -167,7 +167,6 @@ export default class Loyalty extends Api {
   ): Promise<ILoyaltyPointsExpirationDate> {
     return this._call("get-points-expiration-date", data, {
       errorMap: {
-        1044: { message: "Associated user account is not active" },
         1045: { message: "Resolve the Card Number - Card not found" },
         1046: { message: "Resolve the Card Number - Card expired" },
         1064: {
@@ -200,7 +199,6 @@ export default class Loyalty extends Api {
   ): Promise<ILoyaltyPointStampAddition> {
     return this._call("addition", data, {
       errorMap: {
-        1044: { message: "Associated user account is not active" },
         1045: { message: "Resolve the Card Number - Card not found" },
         1046: { message: "Resolve the Card Number - Card expired" },
         1064: { message: "Resolve the User Partner Ext Id error. ( with user_id_type => 'partner_ext_id' )" },
@@ -234,7 +232,6 @@ export default class Loyalty extends Api {
   ): Promise<ILoyaltyPointDeduction> {
     return this._call("deduction", data, {
       errorMap: {
-        1044: { message: "Associated user account is not active" },
         1045: { message: "Resolve the Card Number - Card not found" },
         1046: { message: "Resolve the Card Number - Card expired" },
         1064: { message: "Resolve the User Partner Ext Id error. ( with user_id_type => 'partner_ext_id' )" },
