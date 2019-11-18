c75a8c9c76cb8e1248aa51c27b474e4d9b7791f9
Martin Petit <martin@igloo.be>
2019-11-18 13:05:44 +0100

    hooks: introduce `useMetadataAnimalUniverses`

---
+++ packages/hooks/src/metadata/useMetadataAnimalUniverses.ts
@@ -0,0 +1,60 @@
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import {
+  MetadataAnimalUniverses,
+  MetadataAnimalUniversesVariables,
+} from "./__generated__/MetadataAnimalUniverses";
+
+export * from "./__generated__/MetadataAnimalUniverses";
+
+const MetadataAnimalUniversesQuery = gql`
+  query MetadataAnimalUniverses($lang: String!) {
+    metadataAnimalUniverses(lang: $lang) {
+      id
+      name
+      species
+    }
+  }
+`;
+
+export const useMetadataAnimalUniverses = (
+  optionsOrLang:
+    | string
+    | {
+        lang: string;
+        type?: string;
+        skip?: boolean;
+      },
+  type?: string,
+) => {
+  const lang =
+    typeof optionsOrLang === "object" ? optionsOrLang.lang : optionsOrLang;
+  const skip = typeof optionsOrLang === "object" ? optionsOrLang.skip : false;
+  type = typeof optionsOrLang === "object" ? optionsOrLang.type : type;
+
+  const res = useQuery<
+    MetadataAnimalUniverses,
+    MetadataAnimalUniversesVariables
+  >(MetadataAnimalUniversesQuery, {
+    variables: {
+      lang,
+    },
+    skip,
+  });
+
+  const data = (res.data && res.data.metadataAnimalUniverses) || [];
+
+  const temp = !!type
+    ? data.filter(d => d.species.toLowerCase().match(type || ""))
+    : data;
+
+  const items = temp.map(d => ({
+    label: d.name,
+    value: d.id,
+  }));
+
+  return {
+    ...res,
+    items,
+  };
+};
