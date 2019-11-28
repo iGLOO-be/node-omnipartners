141cf49807547ed0c16d19d4eab9cc43849d78c6
Martin Petit <martin@igloo.be>
2019-11-28 12:49:10 +0100

    hooks + graphql-schema: list partner types

---
+++ packages/hooks/src/metadata/__generated__/MetadataPartnerTypes.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MetadataPartnerTypes
+// ====================================================
+
+export interface MetadataPartnerTypes_metadataPartnerTypes {
+  __typename: "PartnerType";
+  code: string;
+  name: string;
+}
+
+export interface MetadataPartnerTypes {
+  metadataPartnerTypes: MetadataPartnerTypes_metadataPartnerTypes[];
+}
+
+export interface MetadataPartnerTypesVariables {
+  lang: string;
+}
