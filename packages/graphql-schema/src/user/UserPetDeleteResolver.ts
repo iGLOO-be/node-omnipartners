2b42faa27d7dc2ed9f3ff9a8264feab941474163
Martin Petit <martin@igloo.be>
2020-04-29 12:51:42 +0200

    graphql-schema: diet recommendation pet management

--- packages/graphql-schema/src/user/UserPetDeleteResolver.ts
+++ packages/graphql-schema/src/user/UserPetDeleteResolver.ts
@@ -1,10 +1,15 @@
-import { IUserPetDeleteInput } from "omnipartners";
+import {
+  IUserPetDeleteInput,
+  IUserPetDietRecommendationDeleteInput,
+} from "omnipartners";
 import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
 import { Context } from "../types/Context";
 import { GenericValidationError } from "../types/GenericValidationError";
 import { User } from "./User";
 import { userDataOptions } from "./UserResolver";
 import { UserResult } from "./UserResult";
+import { UserPet } from "./UserPet";
+import { UserPetUpdateResult } from "./UserPetUpdateResult";
 
 @InputType()
 class UserPetDeleteInput {
@@ -13,6 +18,24 @@ class UserPetDeleteInput {
 
   @Field()
   public petDeletionCause?: string;
+}
+@InputType()
+class UserPetDeleteDietRecommendationInput
+  implements IUserPetDietRecommendationDeleteInput {
+  @Field()
+  public pet_guid!: string;
+
+  @Field({ nullable: true })
+  public creation_date?: string;
+
+  @Field({ nullable: true })
+  public collection_reference?: string;
+
+  @Field({ nullable: true })
+  public partner_ext_id?: string;
+
+  @Field({ nullable: true })
+  public custom_logger_info?: string;
 }
 
 const mapClixrayFields = (userPetInput: UserPetDeleteInput) => {
@@ -58,6 +81,50 @@ export class UserPetDeleteResolver {
       });
     } catch (err) {
       return new UserResult({
+        error: new GenericValidationError(err),
+      });
+    }
+  }
+  @Mutation(() => UserPetUpdateResult, { nullable: false })
+  public async userPetDeleteDietRecommendation(
+    @Ctx() ctx: Context,
+    @Arg("token") token: string,
+    @Arg("userPetDeleteDietrecommendationInput")
+    userPetDeleteDietrecommendationInput: UserPetDeleteDietRecommendationInput,
+  ): Promise<UserPetUpdateResult> {
+    const { user_guid } = ctx.userTokenHelper.parse(token);
+    const pet = (
+      await ctx.omnipartners.identity.getPet({
+        pet_guid: userPetDeleteDietrecommendationInput.pet_guid,
+      })
+    ).data;
+
+    if (pet.pet_owner.user_guid !== user_guid) {
+      // TODO: better error
+      throw new Error("Not your pet!");
+    }
+
+    try {
+      await ctx.omnipartners.identity.deletePetDietRecommendation(
+        userPetDeleteDietrecommendationInput,
+      );
+      const updatedPet = (
+        await ctx.omnipartners.identity.getPet({
+          pet_guid: userPetDeleteDietrecommendationInput.pet_guid,
+        })
+      ).data;
+      const user = await ctx.omnipartners.identity.authenticateByGUID({
+        data_options: userDataOptions,
+        user_guid,
+      });
+      return new UserPetUpdateResult({
+        result: {
+          pet: new UserPet(updatedPet),
+          user: new User(user),
+        },
+      });
+    } catch (err) {
+      return new UserPetUpdateResult({
         error: new GenericValidationError(err),
       });
     }
